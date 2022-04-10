3cc2cb3f62c6bc9710b77bc28a9106daf33cf444
Nguyễn Tuấn Ngọc <72445778+tuanngoc2001@users.noreply.github.com>
2022-04-10 22:19:40 +0700

    migration

--- Web.Data/EF/ImDbContext.cs
+++ Web.Data/EF/ImDbContext.cs
@@ -8,7 +8,7 @@ namespace Web_Data
         public ImDbContext (DbContextOptions<ImDbContext> options) : base(options)
         {
         }
-        private static string ConnectionString= Util.GetConfig("ConnectionStrings:MyDb");
+        
         public virtual DbSet<BinhLuan> im_Comment { get; set; }
         public virtual DbSet<ChiTietHoaDon> im_Invoice_Detail { get; set; }
         public virtual DbSet<DanhMuc> im_Category { get; set; }
@@ -17,11 +17,7 @@ namespace Web_Data
         public virtual DbSet<NhaCungCap> im_Supplier { get; set; }
         public virtual DbSet<SanPham> im_Product { get; set; }
         public virtual DbSet<User> im_User { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(ConnectionString);
-        }
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
---
+++ Web.Data/EF/ImDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Data.EF
+{
+    public class ImDbContextFactory : IDesignTimeDbContextFactory<ImDbContext>
+
+    {
+        public ImDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<ImDbContext>();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyDb"));
+            return new ImDbContext(optionsBuilder.Options);
+        }
+    }
+}
